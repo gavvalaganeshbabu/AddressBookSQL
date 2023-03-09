38c7109af2ce4268188bf20eb077823da4e2a332
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-03-09 18:26:24 +0530

    Made TErissa

---
+++ Emp-Payroll-UC10.sql.sql
@@ -0,0 +1,5 @@
+﻿------- UC 10: Adding another Value for Rujula in Editing Department -------
+
+Insert into employee_payrolls(name,BasicPay,StartDate,Address,EmployeePhoneNumber,EmployeeDepartment) values ('Manu',250000,'2019-04-20','Bombay,Maharashtra','9550688557','Editing');
+
+select * from employee_payrolls;
