428683ec1988d4e04dc7cf2f5619f0c1867f57f4
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-03-09 18:04:37 +0530

    program File Added

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Employee_Payroll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee-Payroll problem!");
+        }
+    }
+}
