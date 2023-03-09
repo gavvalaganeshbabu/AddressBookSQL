240b5593f1fa7da72559b9b6669a65ed3eda7b3e
gavvalaganeshbabu <ganeshgavvala@gmail.com>
2023-03-09 18:14:31 +0530

    Retrieving Data from table operation Added

---
+++ Emp-Payroll-UC4.sql.sql
@@ -0,0 +1,9 @@
+﻿-----UC-3 Insert values in Table-----
+Insert into employee_payrolls(name,salary,startDate) values
+('Sita',20000,'2021-03-12'),
+('Gita',25000,'2021-04-18'),
+('Nita',10000,'2020-05-13'),
+('Rita',30000,'2020-08-19');
+
+--UC-4 Retrieve All data--
+select * from employee_payrolls;
